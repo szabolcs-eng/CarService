7798838913408ab125d8367983495825d56b3c65
szabolcs <pancelszabolcs@gmail.com>
2026-06-11 15:12:51 +0200

    IdentityDbContext added

---
+++ Backend/CarServiceApi/CarServiceApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using CarServiceApi.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace CarServiceApi.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<ServiceLog> ServiceLogs { get; set; }
+        public DbSet<FuelLog> FuelLogs { get; set; }    
+
+    }
+}
--- Backend/CarServiceApi/CarServiceApi/Models/FuelLog.cs
+++ Backend/CarServiceApi/CarServiceApi/Models/FuelLog.cs
@@ -4,10 +4,10 @@
     {
         public int Id { get; set; }
         public int VehicleId { get; set; }
+        public Vehicle? Vehicle { get; set; }
         public DateTime Date { get; set; }
         public int CarKmCount { get; set; }
         public int FuelAmount { get; set; } 
         public int FuelCost { get; set; }
-
     }
 }
