d951f8f6acb6bd69f45b2c8029e4ea22bcbe1b11
szabolcs <pancelszabolcs@gmail.com>
2026-06-11 15:02:56 +0200

    add Models classes

---
+++ Backend/CarServiceApi/CarServiceApi/Models/ServiceLog.cs
@@ -0,0 +1,12 @@
+﻿namespace CarServiceApi.Models
+{
+    public class ServiceLog
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public DateTime Date { get; set; }
+        public int CarKmCount { get; set; }
+        public string ServiceDescription { get; set; } = string.Empty;
+        public int ServiceCost { get; set; }
+    }
+}
