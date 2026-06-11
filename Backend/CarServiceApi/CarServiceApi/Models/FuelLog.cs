0431b57c01166f5ef1f804795d404b9691e4422a
szabolcs <pancelszabolcs@gmail.com>
2026-06-11 15:02:56 +0200

    add Models classes

---
+++ Backend/CarServiceApi/CarServiceApi/Models/FuelLog.cs
@@ -0,0 +1,12 @@
+﻿namespace CarServiceApi.Models
+{
+    public class FuelLog
+    {
+        public int Id { get; set; }
+        public int VehicleId { get; set; }
+        public DateTime Date { get; set; }
+        public int CarKmCount { get; set; }
+        public int FuelAmount { get; set; } 
+        public int FuelCost { get; set; }
+    }
+}
---
+++ Backend/CarServiceApi/CarServiceApi/Models/Vehicle.cs
@@ -0,0 +1,12 @@
+﻿namespace CarServiceApi.Models
+{
+    public class Vehicle
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string LicensePlate { get; set; } = string.Empty;
+        public string Brand { get; set; } = string.Empty; 
+        public string Model { get; set; } = string.Empty;
+        public int Year { get; set; }  
+    }
+}
