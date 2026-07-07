685baa59b8e67abea6957387e716132e03108cfa
szabolcs <pancelszabolcs@gmail.com>
2026-07-07 10:31:54 +0200

    clean up the code

--- Backend/CarServiceApi/CarServiceApi/Program.cs
+++ Backend/CarServiceApi/CarServiceApi/Program.cs
@@ -1,7 +1,6 @@
 using CarServiceApi.Data;
 using CarServiceApi.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
