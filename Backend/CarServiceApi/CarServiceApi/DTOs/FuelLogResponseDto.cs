8f18bbde4217663281806dcaf7d86fb74cbea864
szabolcs <pancelszabolcs@gmail.com>
2026-07-13 14:20:14 +0200

    Create: New ResponseDto-s

---
+++ Backend/CarServiceApi/CarServiceApi/DTOs/FuelLogResponseDto.cs
@@ -0,0 +1,11 @@
+﻿namespace CarServiceApi.DTOs
+{
+    public record FuelLogResponseDto(
+        int Id,
+        int VehicleId,
+        DateTime Date,
+        int CarKmCount,
+        double FuelAmount,
+        decimal FuelCost
+    );
+}
