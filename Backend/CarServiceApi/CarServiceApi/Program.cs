8749646dd01681dcab5bd9b7275c593c721ba219
szabolcs <pancelszabolcs@gmail.com>
2026-07-22 15:16:18 +0200

    Refactor db for cloud db

--- Backend/CarServiceApi/CarServiceApi/Program.cs
+++ Backend/CarServiceApi/CarServiceApi/Program.cs
@@ -103,8 +103,6 @@ using (var scope = app.Services.CreateScope())
     try
     {
         var context = services.GetRequiredService<ApplicationDbContext>();
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
         context.Database.Migrate();
 
         if (!context.Users.Any(u => u.Role == "Admin"))
